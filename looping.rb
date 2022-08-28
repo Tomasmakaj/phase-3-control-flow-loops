81a8f54c84cf769eae8121e6c6a42e4f865eec95
Tomas Makaj <Tmakajcoding@gmail.com>
2022-08-28 13:06:05 -0400

    commit tree 6260c09f346994e98630c98c97aceb080cd55a29

--- looping.rb
+++ looping.rb
@@ -1,5 +1,23 @@
+# /*
+#   Write a method `happy_new_year` that outputs numbers starting at 10 and
+#    counting down to 1. After reaching 1, print out "Happy New Year!"
+# */
+# function happyNewYear() {
+#   let counter = 10;
+#   while (counter > 0) {
+#     console.log(counter);
+#     counter--;
+#   }
+#   console.log("Happy New Year!");
+# }
+
 def happy_new_year
-  # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
